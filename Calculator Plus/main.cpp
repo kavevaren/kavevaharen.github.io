10b86c517cb4f4903bc4cc3c83a73b4262d16be2
kaveva <90782238+kavevaren@users.noreply.github.com>
2024-06-12 15:31:31 +0700

    Add files via upload

---
+++ Calculator Plus/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+int main() {
+
+    short number ;
+    short number_2;
+    cin >> number;
+    cin >> number_2 ;
+    cout << number + number_2;
+    return 0;
+
+
+}
