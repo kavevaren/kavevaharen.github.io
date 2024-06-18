326491795e131b3e4aa92d159e1e8021fe46ea7b
kaveva <90782238+kavevaren@users.noreply.github.com>
2024-06-18 12:25:03 +0700

    Add files via upload

---
+++ Check The Size Of Variables/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+using namespace std;
+int main() {
+    cout<<"Integer "<<sizeof(int)<<" bit"<<endl;
+    cout<<"Character "<<sizeof(char)<<" bit"<<endl;
+    cout<<"Double" << sizeof(double)<<" bit "<<endl;
+    return 0;
+}
